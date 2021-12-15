60f02d7c7815f33821dbf645c1b29bc2113c2b8b
Triz <kiebusiness1@gmail.com>
2021-12-15 00:01:34 +0000

    Initial commit

---
+++ 2021/Day1.js
@@ -0,0 +1,32 @@
+import { AoCCore } from "../AoCCore.js";
+
+export class Day01 extends AoCCore
+{
+    constructor()
+    {
+        super('1')
+    }
+
+    async solve()
+    {
+        const input = (await this.getFile()).split('\r\n');
+        let count = 0;
+
+        // Part 1
+        for (let i = 1; i < input.length; i++)
+            if (parseInt(input[i]) > parseInt(input[i - 1]))
+                count++
+        this.lap(count)
+
+        // Part 2
+        let sums = [];
+        for (let i = 2; i < input.length; i++)
+            sums.push(parseInt(input[i]) + parseInt(input[i - 1]) + parseInt(input[i - 2]))
+
+        count = 0;
+        for (let i = 1; i < sums.length; i++)
+            if (sums[i] > sums[i - 1])
+                count++
+        this.lap(count)
+    }
+}
---
+++ AoCCore.js
@@ -0,0 +1,36 @@
+import { promises as fs } from 'fs';
+
+export class AoCCore
+{
+    part = 1;
+    timerStart;
+
+    constructor(day)
+    {
+        this.timerStart = Date.now();
+        this.day = day;
+    }
+
+    async getFile()
+    {
+        const file = await fs.readFile(`./2021/Inputs/day${this.day}.txt`, 'utf-8');
+        return file;
+    }
+
+    lap(answer)
+    {
+        let timeSpent = (Date.now() - this.timerStart) / 1000;
+        console.log(`Part ${this.part}: ${answer} (${this.timeToString(timeSpent)})`);
+        this.timerStart = Date.now();
+        this.part++;
+    }
+
+    timeToString(time)
+    {
+        if (time < 1000)
+            return time + "µs";
+        if (time < 1000000)
+            return (time / 1000.0) + "ms";
+        return (time / 1000000.0) + "s";
+    }
+}
