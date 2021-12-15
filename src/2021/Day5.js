c9f727c67fab3096bdfe9ba35c80aad05ef5661b
Triz <kiebusiness1@gmail.com>
2021-12-15 19:27:17 +0000

    Skipped Day 5

---
+++ src/2021/Day5.js
@@ -0,0 +1,15 @@
+import { AoCCore } from "../AoCCore.js";
+
+export class Day05 extends AoCCore
+{
+    constructor()
+    {
+        super('_ex/5')
+    }
+
+    async solve()
+    {
+        const input = (await this.getFile()).split(this.lineSplit);
+        // Skipping this for now.
+    }
+}
