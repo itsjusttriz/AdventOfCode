327a14464d13f92378d7e08ce5efc2b187230e1a
Triz <kiebusiness1@gmail.com>
2021-12-17 03:07:14 +0000

    Complete Day 6

--- src/2021/Day6.js
+++ src/2021/Day6.js
@@ -13,18 +13,48 @@ export class Day06 extends AoCCore
 
         let fishMap = [];
 
-        for (const s of input[0].split(','))
-            fishMap.push(new Fish(parseInt(s)))
+        for (const n of input[0].split(','))
+            fishMap.push(new Fish(parseInt(n)))
 
         // Part 1
-        let copy = [...fishMap]
+        let copy = JSON.parse(JSON.stringify(fishMap));
         for (let i = 0; i < 80; i++)
             this.grow(copy)
 
         this.lap(copy.length)
 
         // Part 2
-        // No idea. Individual fish are too hard to track
+        let copy2 = JSON.parse(JSON.stringify(fishMap));
+        let sum = 0;
+        let map = [];
+        for (let x = 0; x < 9; x++)
+            map[x] = copy2.filter(f => f.timer === x).length;
+
+        for (let x = 0; x < 256; x++)
+            this.grow2(map)
+
+        for (let c of Object.values(map))
+            sum += c
+        this.lap(sum)
+    }
+
+    grow2(map)
+    {
+        const newFish = map[0]
+        for (let i = 0; i < map.length; i++)
+        {
+            switch (i)
+            {
+                case 6:
+                    map[i] = map[i + 1] + newFish
+                    break;
+                case 8:
+                    map[i] = newFish
+                    break;
+                default:
+                    map[i] = map[i + 1]
+            }
+        }
     }
 
     grow(fish)
