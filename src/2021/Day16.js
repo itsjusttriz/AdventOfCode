29972774199289a4a8e535aba210871b84d21655
Triz <kiebusiness1@gmail.com>
2021-12-17 03:06:59 +0000

    Prepare Day 16

---
+++ src/2021/Day16.js
@@ -0,0 +1,14 @@
+import { AoCCore } from "../AoCCore.js";
+
+export class Day16 extends AoCCore
+{
+    constructor()
+    {
+        super('16')
+    }
+
+    async solve()
+    {
+        const input = (await this.getFile()).split(this.lineSplit);
+    }
+}
