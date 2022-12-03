99dc69eaa04af5108beea9d4a76d60216f8040d9
itsjusttriz <itsjusttriz@gmail.com>
2022-12-03 21:51:11 +0000

    Completed Day 3.

--- src/2022/AoC_2022.js
+++ src/2022/AoC_2022.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { Day01 } from './puzzles/Day01.js';
 import { Day02 } from './puzzles/Day02.js';
+import { Day03 } from './puzzles/Day03.js';
 
 class AoC2022 {
     constructor() {
         // new Day01();
-        new Day02();
+        // new Day02();
+        new Day03();
     }
 }
 
---
+++ src/2022/puzzles/Day03.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import { AoCCore } from '../../AoCCore.js';
+
+export class Day03 extends AoCCore {
+    constructor() {
+        super({
+            day: '03', year: '2022'
+        });
+        this.solve();
+    }
+
+    async solve() {
+        const input = (await this.getFile()).split(this.lineSplit);
+
+        let letterMap = {};
+        let lowerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+        let upperLetters = lowerLetters.map(l => l.toUpperCase());
+
+        for (let i = 1; i < (lowerLetters.length + upperLetters.length); i++) {
+            const lower = lowerLetters[i - 1];
+            const upper = upperLetters[i - 1];
+
+            letterMap[lower] = i;
+            letterMap[upper] = i + 26;
+        }
+
+        let sum = 0;
+        let sum2 = 0;
+
+        // Part 1
+        for (const rucksack of input) {
+            const side1 = rucksack.slice(0, rucksack.length / 2).split('');
+            const side2 = rucksack.slice(rucksack.length / 2).split('');
+
+            const common = side1.filter(letter => side2.includes(letter))[0];
+            sum += letterMap[common];
+        }
+
+        // Part 2
+        for (let j = 0; j < input.length; j += 3) {
+            const [elf1, elf2, elf3] = [input[j].split(''), input[j + 1].split(''), input[j + 2].split('')];
+            const common = elf1.filter(letter => elf2.includes(letter) && elf3.includes(letter))[0];
+            sum2 += letterMap[common];
+        }
+        this.lap(sum);
+        this.lap(sum2);
+    }
+}
