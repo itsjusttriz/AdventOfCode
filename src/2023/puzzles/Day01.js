e9df824b45a3ac485ff9759a1b10e59db5a06feb
Triz <kiebusiness1@gmail.com>
2023-12-01 10:16:31 +0000

    Completed Day 1 - 2023

---
+++ src/2023/puzzles/Day01.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+import { AoCCore } from "../../AoCCore.js";
+
+export class Day01 extends AoCCore {
+    constructor() {
+        super({ day: "01", testing: false });
+    }
+
+    /**
+     *
+     * @param {string[]} input
+     */
+    async solve(input) {
+        input = (await input).split(this.lineSplit);
+
+        // PART 1
+        const filteredInput = [...input].map((line) => line.split("").filter(Number));
+        const numArray = [];
+
+        for (const list of filteredInput) numArray.push(list[0] + list.at(-1));
+
+        const resultOne = numArray.map(Number).reduce((total, curr) => total + curr, 0);
+        this.lap(resultOne);
+
+        // PART 2 - Credit to TurkeyDev for help with this!
+        const resultTwo = [...input]
+            .map((line) => {
+                const reg = /(\d|one|two|three|four|five|six|seven|eight|nine)/g;
+                const lineCopy = line.slice();
+
+                const matchOne = this.convertToInts(lineCopy.match(reg)[0]);
+
+                let idx = lineCopy.length - 1;
+                while (!lineCopy.substring(idx).match(reg)) {
+                    idx--;
+                }
+                const matchTwo = this.convertToInts(lineCopy.substring(idx).match(reg)[0]);
+
+                return parseInt(matchOne + matchTwo);
+            })
+            .reduce((total, curr) => total + curr, 0);
+        this.lap(resultTwo);
+    }
+
+    convertToInts(string) {
+        return string
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9");
+    }
+}
