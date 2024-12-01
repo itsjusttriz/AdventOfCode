eb868fdba15602673bc40cf747d61bda384efd8e
itsjusttriz <kiebusiness1@gmail.com>
2024-12-01 17:00:08 +0000

    chore(2015): cleanup

--- src/2015/AoC_2015.js
+++ src/2015/AoC_2015.js
@@ -1,10 +1,7 @@
 import { Day01 } from "./puzzles/Day01.js";
 
-
-class AoC2015 {
+new (class AoC2015 {
     constructor() {
         new Day01();
     }
-}
-
-new AoC2015();
+})();
--- src/2015/puzzles/Day01.js
+++ src/2015/puzzles/Day01.js
@@ -2,34 +2,27 @@ import { AoCCore } from "../../AoCCore.js";
 
 export class Day01 extends AoCCore {
     constructor() {
-        super('01', '2015')
-        // super('_ex/01', '2015')
-        this.solve();
+        super({ day: "01", year: "2015", testing: false });
     }
 
     async solve() {
-        const input = (await this.getFile()).split('');
+        const input = (await this.getFile()).split("");
 
         // Part 1
         let floor = 0;
         for (const direction of input) {
-            if (direction === '(')
-                floor++;
-            if (direction === ')')
-                floor--;
+            if (direction === "(") floor++;
+            if (direction === ")") floor--;
         }
-        this.lap(floor)
+        this.lap(floor);
 
         // Part 2
         floor = 0;
         for (let i = 0; i < input.length; i++) {
-            if (input[i] === '(')
-                floor++;
-            else
-                floor--;
+            if (input[i] === "(") floor++;
+            else floor--;
 
-            if (floor === -1)
-                return this.lap(i + 1)
+            if (floor === -1) return this.lap(i + 1);
         }
     }
 }
