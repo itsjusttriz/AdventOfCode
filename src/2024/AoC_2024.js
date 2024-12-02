df1e739e037992b9155a87933bc567e011884ced
itsjusttriz <kiebusiness1@gmail.com>
2024-12-02 02:32:03 +0000

    chore(2024): Prep

---
+++ src/2024/AoC_2024.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { Day01 } from "./puzzles/Day01.js";
+
+new (class {
+    constructor() {
+        new Day01();
+    }
+})();
---
+++ src/2024/puzzles/Day01.js
@@ -0,0 +1,19 @@
+import { AoCCore } from "../../AoCCore.js";
+
+export class Day01 extends AoCCore {
+    constructor() {
+        super({
+            day: "01",
+            year: "2024",
+            testing: true,
+        });
+    }
+
+    /**
+     *
+     * @param {Promise<string>} input
+     */
+    async solve(input) {
+        input = await input;
+    }
+}
