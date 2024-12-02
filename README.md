bc00e61f1e8e1af58e20fe03319a8646c6d67b4a
itsjusttriz <kiebusiness1@gmail.com>
2024-12-02 02:33:21 +0000

    chore(core): add/clean background files

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Advent of Code 2021
 
-## __Opinion__
+## **Opinion**
 
     So...
-        
+
         ...much...
 
                 ...Fun = |
-*- Triz 15/12/2021*
+
+_- Triz 15/12/2021_
 
 <br/>
 
-## __Progress__
+## **Progress**
 
-| Event | Click to view |
-|-------|---------------
+| Event | Click to view                                                                    |
+| ----- | -------------------------------------------------------------------------------- |
 | 2015  | [View](https://github.com/itsjusttriz/AdventOfCode/blob/main/src/2015/README.md) |
 | 2021  | [View](https://github.com/itsjusttriz/AdventOfCode/blob/main/src/2021/README.md) |
 | 2022  | [View](https://github.com/itsjusttriz/AdventOfCode/blob/main/src/2022/README.md) |
+| 2023  | [View](https://github.com/itsjusttriz/AdventOfCode/blob/main/src/2023/README.md) |
+| 2024  | [View](https://github.com/itsjusttriz/AdventOfCode/blob/main/src/2024/README.md) |
---
+++ create-day-files.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <your_variable>"
+    exit 1
+fi
+
+# Access the first command-line argument as a variable
+day="0$1"
+year="2024"
+
+# Create a file with a JS class setup
+echo "/* eslint-disable no-unused-vars */
+import { AoCCore } from '../../AoCCore.js';
+
+export class Day$day extends AoCCore {
+    constructor() {
+        super({ day: '$day', year: '$year', testing: true });
+    }
+
+    /**
+     *
+     * @param {string[]} input
+     */
+    async solve(input) {
+        input = (await input).split(this.lineSplit);
+
+        const resultOne = await this.part1(input);
+        this.lap(resultOne);
+
+        const resultTwo = await this.part2(input);
+        this.lap(resultTwo);
+    }
+
+    async part1(input) {}
+
+    async part2(input) {}
+}
+" > src/$year/puzzles/Day$day.js
+echo "Created File: src/$year/puzzles/Day$day.js"
+
+# Create two empty text files
+touch src/$year/assets/day$day.txt
+echo "Created File: src/$year/assets/day$day.txt"
+
+touch src/$year/assets/day_ex/day$day.txt
+echo "Created File: src/$year/assets/day_ex/day$day.txt"
+
+# Modify Init Class
+echo "/* eslint-disable no-unused-vars */
+import { Day$day } from './puzzles/Day$day.js';
+
+class AoC$year {
+    constructor() {
+        new Day$day();
+    }
+}
+
+new AoC2023();" > src/$year/AoC_$year.js
+
+echo "Modified File: src/$year/Aoc_$year.js"
