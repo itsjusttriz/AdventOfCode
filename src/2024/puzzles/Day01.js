388d3937256e4c6de7254ae8867a21a64800660e
itsjusttriz <kiebusiness1@gmail.com>
2024-12-07 02:08:19 +0000

    feat(2024): add day 01

--- src/2024/puzzles/Day01.js
+++ src/2024/puzzles/Day01.js
@@ -5,7 +5,7 @@ export class Day01 extends AoCCore {
         super({
             day: "01",
             year: "2024",
-            testing: true,
+            testing: false,
         });
     }
 
@@ -14,6 +14,30 @@ export class Day01 extends AoCCore {
      * @param {Promise<string>} input
      */
     async solve(input) {
-        input = await input;
+        input = (await input()).split(this.lineSplit).map((s) => s.split(" ").filter(Boolean));
+
+        let inputCopy = [...input];
+
+        const leftList = inputCopy.map((item) => item[0]).sort();
+        const rightList = inputCopy.map((item) => item[1]).sort();
+
+        let sum = 0;
+
+        // PART ONE
+        for (let i = 0; i < leftList.length; i++) {
+            sum += Math.abs(leftList[i] - rightList[i]);
+        }
+
+        this.lap(sum);
+
+        sum = 0;
+
+        // PART TWO
+        for (let i = 0; i < leftList.length; i++) {
+            let num = leftList[i];
+            let occurCount = rightList.filter((n) => n === num).length;
+            sum += num * occurCount;
+        }
+        this.lap(sum);
     }
 }
