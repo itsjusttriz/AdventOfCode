f75249c8532a213db38606b9c72f4233e0cd6db5
itsjusttriz <kiebusiness1@gmail.com>
2025-04-04 11:58:07 +0100

    forgot to add actual puzzle files to day02

---
+++ src/2024/puzzles/Day02.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { AoCCore } from '../../AoCCore.js';
+
+export class Day02 extends AoCCore {
+    constructor() {
+        super({
+            day: '02',
+            year: '2024',
+            testing: true
+        });
+    }
+
+    /**
+     *
+     * @param {Promise<string>} input
+     */
+    async solve(input) {
+        input = (await input).split(this.lineSplit);
+
+        // PART ONE
+        // PART TWO
+    }
+}
+
